4230ed9a2e9a1e220d44012db1da7800c6e8579e
Roman Merkushin <rmerkushin@ya.ru>
2014-04-25 14:27:39 +0700

    add new keywords

--- src/OracleLibrary/query.py
+++ src/OracleLibrary/query.py
@@ -2,16 +2,18 @@
 # Copyright (c) 2013 Roman Merkushin
 # rmerkushin@ya.ru
 
+import time
 from robot.api import logger
+
 
 class Query(object):
     """
     Query handles all the querying done by the Oracle Library.
     """
 
-    def query(self, selectStatement):
+    def query(self, select_statement):
         """
-        Executes the selectStatement as SQL command and return query result.\n
+        Executes the select_statement as SQL command and return query result.\n
         Example usage:
         | @{queryResult} | Query | SELECT * FROM NLS_DATABASE_PARAMETERS |
         | Log Many | @{queryResult} |
@@ -19,25 +21,25 @@ class Query(object):
         cursor = None
         try:
             cursor = self.connection.cursor()
-            self.__execute_sql(cursor, selectStatement)
-            allRows = cursor.fetchall()
+            self.__execute_sql(cursor, select_statement)
+            all_rows = cursor.fetchall()
             i = 0
-            for x in allRows:
-                allRows[i] = map(lambda s: str(s).decode("utf-8") if s else '', x)
+            for x in all_rows:
+                all_rows[i] = map(lambda s: str(s).decode("utf-8") if s else '', x)
                 i += 1
-            return allRows
+            return all_rows
         finally:
             if cursor:
                 self.connection.rollback()
 
-    def query_result_as_one_row(self, selectStatement):
+    def query_result_as_one_row(self, select_statement):
         cursor = None
         try:
             cursor = self.connection.cursor()
-            self.__execute_sql(cursor, selectStatement)
-            allRows = cursor.fetchall()
+            self.__execute_sql(cursor, select_statement)
+            all_rows = cursor.fetchall()
             result = []
-            for row in allRows:
+            for row in all_rows:
                 for x in row:
                     result.append(str(x).decode("utf-8"))
             return result
@@ -45,9 +47,9 @@ class Query(object):
             if cursor:
                 self.connection.rollback()
 
-    def row_count(self, selectStatement):
+    def row_count(self, select_statement):
         """
-        Uses the input `selectStatement` to query the database and returns the number of rows from the query.\n
+        Uses the input `select_statement` to query the database and returns the number of rows from the query.\n
         Example usage:
         | ${rowCount} | Row Count | SELECT * FROM NLS_DATABASE_PARAMETERS |
         | Log Many | ${rowCount} |
@@ -55,7 +57,7 @@ class Query(object):
         cursor = None
         try:
             cursor = self.connection.cursor()
-            self.__execute_sql(cursor, selectStatement)
+            self.__execute_sql(cursor, select_statement)
             cursor.fetchall()
             return cursor.rowcount
         finally:
@@ -116,6 +118,39 @@ class Query(object):
         finally:
             if cursor:
                 self.connection.rollback()
+
+    def wait_until_exists_in_database(self, select_statement, timeout=10):
+        cursor = self.connection.cursor()
+        for i in range(timeout):
+            self.__execute_sql(cursor, select_statement)
+            cursor.fetchall()
+            if cursor.rowcount != 0:
+                return True
+            time.sleep(1)
+        raise AssertionError("Record was not found in %s second(s)." % str(timeout))
+
+    def wait_until_not_exists_in_database(self, select_statement, timeout=10):
+        cursor = self.connection.cursor()
+        for i in range(timeout):
+            self.__execute_sql(cursor, select_statement)
+            cursor.fetchall()
+            if cursor.rowcount == 0:
+                return True
+            time.sleep(1)
+        raise AssertionError("Record was not deleted in %s second(s)." % str(timeout))
+
+    def dump_table(self, select_statement):
+        table = '<table border="1">'
+        cursor = self.connection.cursor()
+        self.__execute_sql(cursor, select_statement)
+        table_body = cursor.fetchall()
+        for row in table_body:
+            table += "<tr>"
+            for cell in row:
+                table += "<td>" + str(cell).decode("utf-8") + "</td>"
+            table += "</tr>"
+        table += "</table>"
+        logger.info(table, html=True)
 
     def __execute_sql(self, cursor, sqlStatement):
         logger.debug("Executing : %s" % sqlStatement)
